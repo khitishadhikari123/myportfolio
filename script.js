a4dc0a42822e50e8f4abe7a0f28b2c9e8a346d4e
Khitish Adhikari <129395370+khitishadhikari123@users.noreply.github.com>
2023-03-30 23:39:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var t= new Typed(".typing",{
+    strings: ["student","designer","learner"],
+    typeSpeed:90,
+    backSpeed:70,
+    loop:true
+})
+
+var t= new Typed(".typing2",{
+    strings: ["Student","Designer","Learner"],
+    typeSpeed:75,
+    backSpeed:55,
+    loop:true
+})
